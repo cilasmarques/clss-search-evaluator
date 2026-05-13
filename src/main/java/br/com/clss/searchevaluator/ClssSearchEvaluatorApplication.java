c8717a13b2b709072c87fae06478a06e6aa96113
Cilas Marques <cilasmrq@gmail.com>
2026-05-13 14:30:36 -0300

    Add initial Spring Boot scaffold

---
+++ src/main/java/br/com/clss/searchevaluator/ClssSearchEvaluatorApplication.java
@@ -0,0 +1,12 @@
+package br.com.clss.searchevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClssSearchEvaluatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClssSearchEvaluatorApplication.class, args);
+    }
+}
